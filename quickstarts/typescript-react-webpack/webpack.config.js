d58bf88794c7e20e465f4dc392e2b4207215b12e
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-07-27 18:22:26 +0200

    chore: update webpack.config.js

--- quickstarts/typescript-react-webpack/webpack.config.js
+++ quickstarts/typescript-react-webpack/webpack.config.js
@@ -1,11 +1,11 @@
 module.exports = {
   mode: "production",
-
+  target: 'node',
   // Enable sourcemaps for debugging webpack's output.
   devtool: "source-map",
 
   resolve: {
-    extensions: [".ts", ".tsx"],
+    extensions: [".ts", ".tsx", ".js", ".jsx"]
   },
 
   module: {
@@ -27,12 +27,7 @@ module.exports = {
       },
     ],
   },
-
-  // When importing a module whose path matches one of the following, just
-  // assume a corresponding global variable exists and use that instead.
-  // This is important because it allows us to avoid bundling all of our
-  // dependencies, which allows browsers to cache those libraries between builds.
-  externals: {
+  external: {
     react: "React",
     "react-dom": "ReactDOM",
   },
