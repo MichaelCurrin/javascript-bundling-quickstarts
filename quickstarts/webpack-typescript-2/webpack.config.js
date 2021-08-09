0b68bc7d0e6678312a9a9ed64c1fbbe3259b1dcd
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-08-09 17:45:51 +0200

    chore: update webpack.config.js and webpack.config.js

--- quickstarts/webpack-typescript-2/webpack.config.js
+++ quickstarts/webpack-typescript-2/webpack.config.js
@@ -1,4 +1,6 @@
 module.exports = {
+  mode: "development",
+
   loaders: [{ test: /\.tsx?$/, loader: "awesome-typescript-loader" }],
   preLoaders: [{ test: /\.js$/, loader: "source-map-loader" }],
 };
