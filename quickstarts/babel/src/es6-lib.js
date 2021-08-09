03813ca38d4510054d328ab2f97aa9c7f45f5170
Michael Currin <18750745+MichaelCurrin@users.noreply.github.com>
2021-08-09 16:15:52 +0200

    feat: update babel

--- quickstarts/babel/src/es6-lib.js
+++ quickstarts/babel/src/es6-lib.js
@@ -1,6 +1,8 @@
 import sum from "lodash/sum";
 
-const double = (number) => number * 2;
-const addFive = (number) => sum([number, 5]);
-
-export { double, addFive };
+export function double(number) {
+  number * 2;
+}
+export function addFive(number) {
+  sum([number, 5]);
+}
